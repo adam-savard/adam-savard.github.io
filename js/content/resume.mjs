00f10e7487cc543bc2703c4d9d81b93adf138d18
Adam Savard <savardfs@gmail.com>
2022-02-13 21:25:55 -0400

    Initial Commit

---
+++ js/content/resume.mjs
@@ -0,0 +1,9 @@
+const resume = `<iframe
+    src="https://drive.google.com/viewerng/viewer?embedded=true&url=http://infolab.stanford.edu/pub/papers/google.pdf#toolbar=0&scrollbar=0"
+    frameBorder="0"
+    scrolling="auto"
+    height="100%"
+    width="100%"
+></iframe>`;
+
+export { resume };
---
+++ js/shortcut/shortcut.mjs
@@ -0,0 +1,87 @@
+import WinBox from 'winbox';
+
+const Shortcut = class {
+    /**
+     * Create a new shortcut to display on the side panel
+     * @param {*} options Needs 3 items: `image` (the source), `windowContent` (an HTML element), `windowOptions` (the options when using the window box plugin)
+     */
+    constructor(
+        options = {
+            image: 'image source',
+            windowContent: document.createElement('div'),
+            windowOptions: {},
+            parent: document.createElement('div'),
+        }
+    ) {
+        /**
+         * The indicator of whether or not this item has been clicked
+         */
+        this.clicked = false;
+
+        /**
+         * Used for setting up the double click; stays at 250ms
+         */
+        this.doubleclickTimeout = null;
+
+        /**
+         * The window content to be displayed by this shortcut
+         */
+        this.windowContent = options.windowContent;
+
+        /**
+         * The windowbox options to use when displaying things
+         */
+        this.windowOptions = options.windowOptions;
+
+        /**
+         * The windowbox to show
+         */
+        this.windowBox = null;
+
+        let anchor = document.createElement('a');
+        anchor.on('click', this.click.bind(this));
+
+        let image = document.createElement('img');
+        image.src = options.image;
+        image.style.maxHeight = '50px';
+        image.style.maxWidth = '50px';
+        anchor.appendChild(image);
+
+        parent.appendChild(anchor);
+    }
+
+    showContent() {
+        if (this.windowBox) {
+            this.windowBox.focus();
+        } else {
+            this.windowBox = new WinBox({
+                ...this.windowOptions,
+                x: 'center',
+                y: 'center',
+                width: '50%',
+                height: '50%',
+                html: this.windowContent,
+                onClose: this.onClose.bind(this),
+            });
+        }
+    }
+
+    onClose() {
+        this.windowBox = null;
+    }
+
+    click() {
+        if (!this.clicked) {
+            let _inst = this;
+            this.clicked = true;
+            this.doubleclickTimeout = setTimeout(function () {
+                _inst.clicked = false;
+            }, 250);
+        } else {
+            clearTimeout(this.doubleclickTimeout);
+            this.showContent();
+        }
+    }
+};
+
+export { Shortcut };
