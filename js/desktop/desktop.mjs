940bdd35a2ee974d0fdc855d88a4f12b0c633e00
Adam Savard <savardfs@gmail.com>
2022-02-13 21:25:48 -0400

    Initial Commit

---
+++ js/desktop/desktop.mjs
@@ -0,0 +1,10 @@
+import { Shortcut } from '../shortcut/shortcut.mjs';
+
+const Desktop = class {
+    /**
+     * Create a 'desktop' that the visitor of the page will be able to interact with
+     */
+    constructor() {
+        let spacing = '10px'; //this is the amount of spacing between the icons
+    }
+};
